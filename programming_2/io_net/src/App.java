426d3f79744aed31d5ad783690e9c48d828a388f
A-Cobra <conradbravina@gmail.com>
2023-06-21 12:11:55 -0500

    The basic structure of the program is comple
    What is missing is to handle the exceptions and provide a good UX

--- programming_2/io_net/src/App.java
+++ programming_2/io_net/src/App.java
@@ -1,12 +1,52 @@
 package programming_2.io_net.src;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Works");
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        URL url = null;
+        final String urlString = "http://";
+        final String fileName = "output.txt";
+
+        // readResourceStream
+        try {
+            url = new URL(urlString);
+            inputStream = url.openStream();
+        } catch (MalformedURLException e) {
+            // TODO: handle exception
+        } catch (IOException e) {
+            // TODO: handle exception
+        }
+
+        // writeResourceStream
+        try {
+            outputStream = new FileOutputStream(fileName);
+        } catch (FileNotFoundException e) {
+            // TODO: handle exception
+        }
+
+        if (inputStream != null && outputStream != null) {
+            // the stream can just be copied if both streams are not null
+            try (InputStream in = inputStream; OutputStream out = outputStream) {
+                copyStream(in, out);
+            } catch (IOException e) {
+                // TODO: handle exception
+            } catch (NullPointerException e) {
+                // TODO: handle exception
+            }
+            // Not using the finally statement because I would have to nest another try
+            // catch block, Therefore, I use a try with resources because it makes sure to
+            // close the streams and makes the code neater to read
+        }
+
     }
 
     private static void copyStream(InputStream in, OutputStream out)
