80842dd9c36615466922efea4ffa7c7dd80b1f5d
A-Cobra <conradbravina@gmail.com>
2023-06-26 18:56:06 -0500

    All the requirements have been taken into account and the basic program flow is done

--- programming_2/sentences_generator/src/App.java
+++ programming_2/sentences_generator/src/App.java
@@ -4,12 +4,19 @@ import programming_2.sentences_generator.utils.StringHandler;
 public class App {
     public static void main(String[] args) throws NoSuchMethodException, SecurityException {
         for (int i = 0; i < 4; i++) {
-            System.out.println(getNounPhrase());
+            System.out.println(getSentence());
         }
     }
 
     private static String getCategory(String categoryType) {
         return GrammaticalCategories.getCategoryByName(categoryType);
+    }
+
+    private static String getSentence() {
+        final String conjunctionAndSentence = getRandomBoolean()
+                ? " " + getCategory("conjunction") + " " + getSentence()
+                : "";
+        return getSimpleSentence() + conjunctionAndSentence;
     }
 
     private static String getNounPhrase() {
@@ -20,11 +27,18 @@ public class App {
         if (probability <= probabilityDivision) {
             return getCategory("properNoun");
         } else {
-            String adjective = (getRandomDouble() > 0.5 ? " " + getCategory("adjective") : "") + ". ";
-            String verbPhrase = getRandomDouble() > 0.5 ? "who " + getVerbPhrase() : "";
+            // In the instructions, a dot (".") appears as the second option for a
+            // noun_phrase, maybe it doesn't belong there, but as the instructions indicate,
+            // I will leave it there
+            String adjective = (getRandomBoolean() ? " " + getCategory("adjective") : "") + ". ";
+            String verbPhrase = getRandomBoolean() ? " who " + getVerbPhrase() : "";
             return getCategory("determiner") + adjective + StringHandler
-                    .toTitleCase(getCategory("commonNoun")) + " " + verbPhrase;
+                    .toTitleCase(getCategory("commonNoun")) + verbPhrase;
         }
+    }
+
+    private static String getSimpleSentence() {
+        return getNounPhrase() + " " + getVerbPhrase();
     }
 
     private static String getVerbPhrase() {
@@ -35,17 +49,19 @@ public class App {
         if (probability <= probabilityDivision) {
             return getCategory("intransitiveVerb");
         } else if (probability <= 2 * probabilityDivision) {
-            return getCategory("transitiveVerb") + " ";
-            // + getNounPhrase();
+            return getCategory("transitiveVerb") + " " + getNounPhrase();
         } else if (probability <= 3 * probabilityDivision) {
             return "is " + getCategory("adjective");
         } else {
-            return "believes that ";
-            // getSimpleSentence();
+            return "believes that " + getSimpleSentence();
         }
     }
 
     private static double getRandomDouble() {
         return Math.random();
+    }
+
+    private static boolean getRandomBoolean() {
+        return getRandomDouble() > 0.5 ? true : false;
     }
 }
