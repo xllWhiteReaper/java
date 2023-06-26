d1778412653c698472338b16f9eebe37a5c356f4
A-Cobra <conradbravina@gmail.com>
2023-06-26 17:35:37 -0500

    Created the first method rule

--- programming_2/sentences_generator/src/App.java
+++ programming_2/sentences_generator/src/App.java
@@ -1,7 +1,37 @@
+import java.util.function.Supplier;
+
 import programming_2.sentences_generator.utils.GrammaticalCategories;
 
 public class App {
     public static void main(String[] args) throws NoSuchMethodException, SecurityException {
-        System.out.println(GrammaticalCategories.getCategoryByName("adjective"));
+        for (int i = 0; i < 4; i++) {
+            System.out.println(getVerbPhrase());
+        }
+    }
+
+    private static String getCategory(String categoryType) {
+        return GrammaticalCategories.getCategoryByName(categoryType);
+    }
+
+    private static String getVerbPhrase() {
+        int possibilities = 4;
+        double probabilityDivision = (double) 1 / possibilities;
+        double probability = getRandomDouble();
+
+        if (probability <= probabilityDivision) {
+            return getCategory("intransitiveVerb");
+        } else if (probability <= 2 * probabilityDivision) {
+            return getCategory("transitiveVerb") + " ";
+            // + getNounPhrase();
+        } else if (probability <= 3 * probabilityDivision) {
+            return "is " + getCategory("adjective");
+        } else {
+            return "believes that ";
+            // getSimpleSentence();
+        }
+    }
+
+    private static double getRandomDouble() {
+        return Math.random();
     }
 }
