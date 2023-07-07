4cf778b51ab041a03dfebde739fa9ee3a66f2b66
A-Cobra <conradbravina@gmail.com>
2023-07-06 22:08:12 -0500

    Created some basic rules, there might be something left

---
+++ programming_2/bnf_set_calculator/src/App.java
@@ -0,0 +1,16 @@
+package programming_2.bnf_set_calculator.src;
+
+import java.util.Scanner;
+// v2, would be interesting <set-result> := <set> <operator> <set> | <set-result> <operator> <set>
+// <set-result> := <set> <operator> <set> | <set>
+// <set> := "[" <set-element> "]"
+// <set-element> := <number> | <set-element> "," <number> | null
+// <operator> := + | * | -
+
+public class App {
+    public static final Scanner Scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("D{ne!}");
+    }
+}
