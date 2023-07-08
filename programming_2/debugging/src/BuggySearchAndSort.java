e1365e58a2bcf14303f002bf1e370145d08a9df1
A-Cobra <conradbravina@gmail.com>
2023-07-08 17:06:15 -0500

    Debugged the insertionSort Algorithm

--- programming_2/debugging/src/BuggySearchAndSort.java
+++ programming_2/debugging/src/BuggySearchAndSort.java
@@ -109,12 +109,15 @@ public class BuggySearchAndSort {
 		for (int top = 1; top < array.length; top++) {
 			int temp = array[top]; // copy item that into temp variable
 			int pos = top - 1;
-			while (pos > 0 && array[pos] > temp) {
+			while (pos >= 0) {
 				// move items that are bigger than temp up one position
-				array[pos + 1] = array[pos];
-				pos--;
+				if(array[pos] > temp) {
+					array[pos + 1] = array[pos];
+					array[pos] = temp; // place temp into last vacated position
+				}
+				pos--;		
+//				if(array[pos] == temp) break;
 			}
-			array[pos] = temp; // place temp into last vacated position
 		}
 	}
 
