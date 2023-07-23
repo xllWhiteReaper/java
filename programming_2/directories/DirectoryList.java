3d76496ac1e46939ea7502fa191917d32408d2d7
A-Cobra <conradbravina@gmail.com>
2023-07-23 13:51:59 -0500

    Cleaned the code

--- programming_2/directories/DirectoryList.java
+++ programming_2/directories/DirectoryList.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 
 /**
  * This program lists the files in a directory specified by
- * the user. The user is asked to type in a directory name.
+ * the user, if there are folders, then it lists the files inside that folder as
+ * well. The user is asked to type in a directory name.
  * If the name entered by the user is not a directory, a
  * message is printed and the program ends.
  */
@@ -13,9 +14,7 @@ public class DirectoryList {
 
       String directoryName; // Directory name entered by the user.
       File directory; // File object referring to the directory.
-      String[] files; // Array of file names in the directory.
       Scanner scanner; // For reading a line of input from the user.
-
       scanner = new Scanner(System.in); // scanner reads from standard input.
 
       System.out.print("Enter a directory name: ");
@@ -24,18 +23,6 @@ public class DirectoryList {
 
       listFilesDirectoriesAndSubFiles(directory, 0);
 
-      // if (!directory.isDirectory()) {
-      // if (!directory.exists())
-      // System.out.println("There is no such directory!");
-      // else
-      // System.out.println("That file is not a directory.");
-      // } else {
-      // files = directory.list();
-      // System.out.println("Files in directory \"" + directory + "\":");
-      // for (int i = 0; i < files.length; i++)
-      // System.out.println(" " + files[i]);
-      // }
-
       scanner.close();
    } // end main()
 
@@ -43,6 +30,7 @@ public class DirectoryList {
       if (nesting < 0) {
          return;
       }
+
       if (!currentDirectory.isDirectory()) {
          if (nesting == 0) {
             if (!currentDirectory.exists())
@@ -55,9 +43,9 @@ public class DirectoryList {
 
       String[] currentDirectoryFilesAndDirectories = currentDirectory.list();
 
-      for (String fileOrDirectory : currentDirectoryFilesAndDirectories) {
-         File possibleDirectory = new File(currentDirectory.getPath() + "/" + fileOrDirectory);
-         TextHelper.printWithIndentation(fileOrDirectory, nesting);
+      for (String currentDirectoryFileOrSubdirectoryName : currentDirectoryFilesAndDirectories) {
+         File possibleDirectory = new File(currentDirectory.getPath() + "/" + currentDirectoryFileOrSubdirectoryName);
+         TextHelper.printWithIndentation(currentDirectoryFileOrSubdirectoryName, nesting);
          if (possibleDirectory.isDirectory()) {
             listFilesDirectoriesAndSubFiles(possibleDirectory, nesting + 2);
          }
